992046b6750027e1c47a1b70ed1c0d5f9aa1df21
Eleazar Nascimento <eleazar.nascimento@gmail.com>
2024-07-30 19:35:42 -0300

    feat: implementa estado de tasks pelo zustand

---
+++ src/store/task.tsx
@@ -0,0 +1,26 @@
+import { create } from "zustand";
+import { ColumnType } from "@/drag-container/Column";
+import { columnData } from "@/mocks/columnData";
+import { CardType } from "@/drag-container/Card";
+
+interface StoreTasksProps {
+  columns: ColumnType[];
+  addTask: (newCard: CardType) => void;
+
+  loading: boolean;
+  updateLoading: (loading: boolean) => void;
+}
+
+export const useStoreTasks = create<StoreTasksProps>((set, get) => ({
+  columns: columnData,
+  addTask: (newCard) => {
+    set({ loading: true });
+    const columns = get().columns;
+
+    columns[0].cards.push(newCard);
+    set({ loading: false });
+  },
+
+  loading: true,
+  updateLoading: (loading: boolean) => set(() => ({ loading })),
+}));
