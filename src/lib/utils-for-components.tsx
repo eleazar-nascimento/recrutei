528e9e1dce50cbdb6f2238a1bb661dea66f421ac
Eleazar Nascimento <eleazar.nascimento@gmail.com>
2024-07-30 21:34:18 -0300

    feat: cria verificação para tratar data e prazo.

---
+++ src/lib/utils-for-components.tsx
@@ -0,0 +1,54 @@
+import { ReactElement } from "react";
+
+export function checkDateStatus(
+  dateString: string,
+  doneTask?: string
+): ReactElement {
+  const inputDate = new Date(dateString);
+
+  const currentDate = new Date();
+
+  inputDate.setHours(0, 0, 0, 0);
+  currentDate.setHours(0, 0, 0, 0);
+
+  const differenceInMilliseconds = inputDate.getTime() - currentDate.getTime();
+
+  const differenceInDays = Math.ceil(
+    differenceInMilliseconds / (1000 * 60 * 60 * 24)
+  );
+
+  if (doneTask === "4") {
+    if (differenceInDays > 0) {
+      return (
+        <span className="font-semibold text-[10px] text-[#63B150]">
+          Dentro do Prazo
+        </span>
+      );
+    } else if (differenceInDays < 0) {
+      return (
+        <span className="font-semibold text-[10px] text-[#E14942]">
+          Fora do Prazo
+        </span>
+      );
+    }
+  }
+  if (differenceInDays > 0) {
+    return (
+      <span className="font-semibold text-[10px] text-[#63B150]">
+        Faltam {differenceInDays} dias.`
+      </span>
+    );
+  } else if (differenceInDays < 0) {
+    return (
+      <span className="font-semibold text-[10px] text-[#E14942]">
+        Atrasado há {Math.abs(differenceInDays)} dias.
+      </span>
+    );
+  } else {
+    return (
+      <span className="font-semibold text-[10px] text-[#63B150]">
+        Hoje é o dia.
+      </span>
+    );
+  }
+}
