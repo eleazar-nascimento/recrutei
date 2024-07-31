44a562b390087e6244b8d98359b1d35f6076ef97
Eleazar Nascimento <eleazar.nascimento@gmail.com>
2024-07-30 21:41:37 -0300

    fix: corrige importação da logo.

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,9 @@
-import Logo from "/src/logo.svg";
+import { LogoImage } from "@/assets/logo";
 
 export function NavBar() {
   return (
     <div className="bg-card p-4 flex items-center justify-start w-full rounded-3xl gap-5">
-      {/* <img src={Logo} alt="Logo da empresa Recrutei" width={56} /> */}
+      <LogoImage />
       <div className="font-semibold text-primary">Teste vaga front</div>
     </div>
   );
